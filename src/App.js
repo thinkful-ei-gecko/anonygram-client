cdef0a8287dd2d25d7c115e826f4d86f6da88438
Phoebe Law <phoebebasilio@gmail.com>
2019-11-25 14:32:49 -0800

    Adds karma-service for karma assignment and updating with timeout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import karmaService from './services/karma-service';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+  componentDidMount() {
+    karmaService.setNewKarma();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>hi</h1>
+          {/* <button onClick={this.refreshState()}>get karma</button> */}
+          <button onClick={() => karmaService.decrementKarma()}>decrement karma</button>
+        </header>
+      </div>
+    );
+  }
+}
