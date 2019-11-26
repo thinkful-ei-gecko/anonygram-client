df67ccd2c61f4798815fdeeda87471100e91a617
Phoebe Law <phoebebasilio@gmail.com>
2019-11-26 12:34:20 -0800

    Edits config.js to account for api endpoint in production

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000',
+  API_ENDPOINT: process.env.NODE_ENV === "production" 
+    ? process.env.REACT_APP_API_ENDPOINT
+    : 'http://localhost:8000/api'
 }
