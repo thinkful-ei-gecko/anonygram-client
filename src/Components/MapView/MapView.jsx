8267ca45f575c2364b86393945805569f73978ce
Scott Schroeder <snschroeder89@gmail.com>
2019-12-03 14:40:02 -0600

    adds zoomLevels to show images based on how zoomed in the user is

--- src/Components/MapView/MapView.jsx
+++ src/Components/MapView/MapView.jsx
@@ -12,13 +12,13 @@ function MapView(props) {
   const [zoom, setZoom] = useState(12);
 
   useEffect(() => {
-      setLoading(true);
-      ImageApi.getLocalImages('new', lat, long)
-          .then((res) => {
-              console.log(res);
-              setImageFeed(res);
-              setLoading(false);
-          })
+    setLoading(true);
+    ImageApi.getLocalImages('new', lat, long)
+      .then((res) => {
+        console.log(res);
+        setImageFeed(res);
+        setLoading(false);
+      })
   }, [lat, long]);
 
   const mapStyles = {
@@ -36,38 +36,56 @@ function MapView(props) {
     console.log(markerImg);
   }
 
-  const generateMarkers = () => {
-
-    let positions = imageFeed.map((point) => {
-      return {lat: point.latitude, long: point.longitude}
-    });
-
-    console.log(positions);
-    
-    let jsx = imageFeed.map((point) => {
-      if (zoom <= 12) {
-
-      }
-    })
+  const truncateCoord = (coord) => {
+    coord.toString();
+    let decimal = coord.indexOf('.');
+    return Number(coord.slice(0, (decimal + 4)))
   }
 
+  const generateMarkers = () => {
+    let positions = imageFeed.map((point) => {
+      const lat = truncateCoord(point.latitude);
+      const long = truncateCoord(point.longitude);
+      return { lat, long, img: point.image_url, karma: point.karma_total, id: point.id, zoomLevel: 12 }
+    });
+
+    for (let i = 0; i < positions.length; i++) {
+      for (let j = i + 1; j < positions.length; j++) {
+        if (Math.abs(positions[i].lat - positions[j].lat) <= 0.01 && Math.abs(positions[i].long - positions[j].long <= 0.01)) {
+          positions[j].zoomLevel = 13;
+        } else if (Math.abs(positions[i].lat - positions[j].lat) <= 0.005 && Math.abs(positions[i].long - positions[j].long <= 0.005)) {
+          positions[j].zoomLevel = 14;
+        }
+      }
+    }
+
+    let jsx = positions.map((point) => {
+      if (zoom >= point.zoomLevel) {
+        return (
+          <Marker
+          key={point.id}
+          icon={{ url: point.img, scaledSize: new props.google.maps.Size(55, 55) }}
+          style={markerStyles}
+          position={{ lat: point.lat, lng: point.long }}
+          onClick={() => handleMarkerClick(point.image_url)}
+        />
+        )
+      }
+    })
+    return jsx;
+  }
+
+  generateMarkers();
+
   return (
-  
-    <Map 
+
+    <Map
       google={props.google}
       zoom={12}
       style={mapStyles}
       initialCenter={{ lat: lat, lng: long }}
     >
-    {imageFeed.map((point) => (
-      <Marker 
-        key={point.id}
-        icon={{url: point.image_url, scaledSize: new props.google.maps.Size(55, 55) }}
-        style={markerStyles}
-        position={{ lat: point.latitude, lng: point.longitude }}
-        onClick={() => handleMarkerClick(point.image_url)}
-      />
-    ))}
+      {generateMarkers()}
     </Map>
   )
 }
