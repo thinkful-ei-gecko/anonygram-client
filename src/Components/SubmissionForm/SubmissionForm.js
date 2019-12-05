1751ee7ac2d4ec624c8ef2a1beebaf9be9655f05
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-12-05 14:15:29 -0500

    install react-dropzone and implement for desktop viewport

--- src/Components/SubmissionForm/SubmissionForm.js
+++ src/Components/SubmissionForm/SubmissionForm.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-
+import Dropzone from 'react-dropzone';
 import './SubmissionForm.css';
-
-
 import config from '../../config';
 
 class SubmissionFrom extends Component {
@@ -15,22 +13,29 @@ class SubmissionFrom extends Component {
     };
   }
 
-  imageSelectHandler = (e) => {
+  imageSelectHandler = e => {
     this.setState({
       image: e.target.files[0],
       nsfwDetected: false,
     });
   };
 
-  onSubmitImageUploader = (e) => {
+  imageDragHandler = file => {
+    this.setState({
+      image: file,
+      nsfwDetected: false,
+    });
+  };
+
+  onSubmitImageUploader = e => {
     e.preventDefault();
 
     //Start loading spinner
     this.setState({ loading: true });
     const formData = new FormData();
     formData.append('someImage', this.state.image);
-    formData.set('latitude', this.props.userLocation.lat)
-    formData.set('longitude', this.props.userLocation.long)
+    formData.set('latitude', this.props.userLocation.lat);
+    formData.set('longitude', this.props.userLocation.long);
     for (var value of formData.values()) {
       console.log(value);
     }
@@ -46,52 +51,87 @@ class SubmissionFrom extends Component {
         if (res.status === 400) {
           this.setState({ nsfwDetected: true });
         }
-        this.setState({ image: null })
+        this.setState({ image: null });
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
       });
   };
 
   resetState = () => {
-    this.setState({ image: null })
-  }
-
+    this.setState({ image: null });
+  };
 
   render() {
-    const { nsfwDetected } = this.state; 
-    return ( 
-      <div>
-        {/* Display loading spinner if loading */}
-        {this.state.loading && <div className='loader'></div>}
-        <section className="nsfw-detected">{nsfwDetected ? 'Sorry, that content is not permitted' : ''}    
-        </section>
-        <form className='SubmissionForm' encType="multipart/form-data" onSubmit={this.onSubmitImageUploader}>
-          
-          <input
-            style={{ display: 'none' }}
-            type="file"
-            accept="image/*"
-            onChange={this.imageSelectHandler}
-            name="someImage"
-            ref={(imageInput) => (this.imageInput = imageInput)}
-          />
-          {
-            (!this.state.image)
-              ? <button type="button"
-                className="SubmissionForm__button"
-                onClick={() => this.imageInput.click()}
-              >+</button>
-              : (
-                <>
-                  <button className='SubmissionForm__button' type="reset" onClick={() => this.resetState()}>Cancel</button>
-                  <button className='SubmissionForm__button' type="submit" value="Upload">Upload</button>
-                </>
-              )
-          }
+    const { nsfwDetected } = this.state;
 
-        </form>
-      </div>  
+    return (
+      <div>
+        <section className="nsfw-detected">
+          {nsfwDetected ? 'Sorry, that content is not permitted' : ''}
+        </section>
+        {/* 
+          component utilizing hooks to detect dropped files 
+          while users are on desktop applications
+           */}
+        <Dropzone onDrop={file => this.imageDragHandler(file[0])}>
+          {({ getRootProps, getInputProps, isDragActive }) => (
+            <form
+              {...getRootProps()}
+              className="SubmissionForm"
+              encType="multipart/form-data"
+              onSubmit={this.onSubmitImageUploader}
+            >
+              <input
+                {...getInputProps()}
+                style={{ display: 'none' }}
+                type="file"
+                accept="image/*"
+                onChange={this.imageSelectHandler}
+                name="someImage"
+                ref={imageInput => (this.imageInput = imageInput)}
+              />
+              {isDragActive ? (
+                <p className="SubmissionForm__drag--active">Nice pic!</p>
+              ) : (
+                !this.state.image && (
+                  <p className="SubmissionForm__drag">
+                    Drag a pic here to upload, or click to select one
+                  </p>
+                )
+              )}
+              {!this.state.image ? (
+                !isDragActive && (
+                  <button
+                    type="button"
+                    className="SubmissionForm__button"
+                    onClick={() => this.imageInput.click()}
+                  >
+                    +
+                  </button>
+                )
+              ) : (
+                <>
+                  <button
+                    className="SubmissionForm__button"
+                    type="reset"
+                    onClick={() => this.resetState()}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="SubmissionForm__button"
+                    type="submit"
+                    value="Upload"
+                  >
+                    Upload
+                  </button>
+                </>
+              )}
+            </form>
+          )}
+        </Dropzone>
+      </div>
     );
   }
 }
