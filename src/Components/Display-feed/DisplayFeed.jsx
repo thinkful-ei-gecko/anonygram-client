ca535a3f2a55673e7736a6350089a44eb3dceb4c
Scott Schroeder <snschroeder89@gmail.com>
2019-12-10 18:01:55 -0600

    determines scroll height and display height on mount and scroll

--- src/Components/Display-feed/DisplayFeed.jsx
+++ src/Components/Display-feed/DisplayFeed.jsx
@@ -1,25 +1,51 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import DisplayItem from './Display-item/DisplayItem';
 import ImageContext from '../../contexts/ImageContext';
-import Paginator from 'react-hooks-paginator';
+// import Paginator from 'react-hooks-paginator';
 
 
 import './DisplayFeed.css';
 
 export default function DisplayFeed(props) {
+    // const [offset, setOffset] = useState(0);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [currentData, setCurrentData] = useState([]);
+    const [scrollHeight, setScrollHeight] = useState(0);
+    const [screenHeight, setScreenHeight] = useState(0);
+    const [pageOffset, setPageOffset] = useState(0);
+
+    const context = useContext(ImageContext);
+
+    const dpFeedRef = useRef(null);
+    const data = context.images; 
+
     useEffect(() => {
         props.setView('feed');
     }, []);
-    //
-    const [offset, setOffset] = useState(0);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [currentData, setCurrentData] = useState([]);
-    const context = useContext(ImageContext);
-    const data = context.images; 
-    console.log(context)
+
     useEffect(() => {
-        setCurrentData(data.slice(offset, offset + pageLimit));
-      }, [offset, data]);
+            setScrollHeight(dpFeedRef.current.scrollHeight);
+            setScreenHeight(window.innerHeight);
+            setPageOffset(window.pageYOffset);
+            console.log('scroll ' + scrollHeight);
+            console.log('height ' + screenHeight);
+            console.log('offset ' + pageOffset);
+    }, [scrollHeight, pageOffset, screenHeight, context.images]);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setPageOffset(window.pageYOffset);
+            console.log(pageOffset)
+        }
+        window.addEventListener('scroll', handleScroll);
+        return (() => {
+            window.removeEventListener('scroll', handleScroll);
+        })
+    }, []);
+
+    // useEffect(() => {
+    //     setCurrentData(data.slice(offset, offset + pageLimit));
+    //   }, [offset, data]);
 
     const generateJSX = () => {
         if (!context.images) {
@@ -27,8 +53,7 @@ export default function DisplayFeed(props) {
         }
         return (
             <>
-            //display currentData 
-                <ul className="img-container">
+                <ul className="img-container" ref={dpFeedRef}>
                     {data.map(imgObj => (
                         <DisplayItem
                             imgAddress={imgObj.image_url}
@@ -40,13 +65,6 @@ export default function DisplayFeed(props) {
                         />
                     ))}
                 </ul> 
-                <Paginator
-                totalRecords={data.length}
-                pageLimit={4}
-                pageNeighbours={1}
-                setOffset={setOffset}
-                currentPage={currentPage}
-                setCurrentPage={setCurrentPage}
                 />         
             </>
         )
